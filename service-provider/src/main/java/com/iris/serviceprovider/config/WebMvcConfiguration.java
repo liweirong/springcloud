1348edb282a5bc5b2135cd60cd1f441c362e2328
liweirong <2277839278@qq.com>
2020-02-06 11:28:16 +0800

    hystrix 原理

--- service-provider/src/main/java/com/iris/serviceprovider/config/WebMvcConfiguration.java
+++ service-provider/src/main/java/com/iris/serviceprovider/config/WebMvcConfiguration.java
@@ -1,15 +1,15 @@
 package com.iris.serviceprovider.config;
 
-//import com.iris.serviceprovider.web.TimeoutAnnotationHandlerInterceptor;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//
-//@Configuration
-//public class WebMvcConfiguration implements WebMvcConfigurer {
-//
-//    public void addInterceptors(InterceptorRegistry registry) {
-//        // 注册一个拦截器
-//        // registry.addInterceptor(new TimeoutAnnotationHandlerInterceptor());
-//    }
-//}
+import com.iris.serviceprovider.web.servlet.TimeoutAnnotationHandlerInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfiguration implements WebMvcConfigurer {
+
+    public void addInterceptors(InterceptorRegistry registry) {
+        // 注册一个拦截器
+         registry.addInterceptor(new TimeoutAnnotationHandlerInterceptor());
+    }
+}
