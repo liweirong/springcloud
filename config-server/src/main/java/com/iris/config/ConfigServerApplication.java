9d08ca12470adc21053b9a2b0f48740a9cde5470
liweirong <2277839278@qq.com>
2020-02-07 15:05:38 +0800

    分布式配置中心 spring cloud config 集群化

--- config-server/src/main/java/com/iris/config/ConfigServerApplication.java
+++ config-server/src/main/java/com/iris/config/ConfigServerApplication.java
@@ -3,9 +3,11 @@ package com.iris.config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableConfigServer //开启config server功能
+@EnableEurekaClient // 开启构建高可用config server
 public class ConfigServerApplication {
 
     public static void main(String[] args) {
