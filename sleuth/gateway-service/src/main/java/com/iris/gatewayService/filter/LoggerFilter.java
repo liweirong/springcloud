207bf7290855d4b0b6beddc95fc0ece7a33b1024
liweirong <2277839278@qq.com>
2020-02-08 14:13:16 +0800

    服务链路追踪 Spring Cloud gateway 链路数据中加入请求的操作人 - 新建zuulFilter过滤器

---
+++ sleuth/gateway-service/src/main/java/com/iris/gatewayService/filter/LoggerFilter.java
@@ -0,0 +1,42 @@
+package com.iris.gatewayService.filter;
+
+import brave.Tracer;
+import com.netflix.zuul.ZuulFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.netflix.zuul.filters.support.FilterConstants;
+import org.springframework.stereotype.Component;
+
+/**
+ * 需求： 在链路请求上加入请求的操作人
+ *
+ * @author iris
+ * @date 2020/2/8
+ */
+@Component
+public class LoggerFilter extends ZuulFilter {
+
+    @Autowired
+    Tracer tracer;
+
+    @Override
+    public String filterType() {
+        return FilterConstants.POST_TYPE;
+    }
+
+    @Override
+    public int filterOrder() {
+        return 900;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() {
+        tracer.currentSpan().tag("operator", "iris");
+        System.out.println("gateway-service loggerFilter :" + tracer.currentSpan());
+        return null;
+    }
+}
